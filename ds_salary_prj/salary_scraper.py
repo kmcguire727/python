0c878bfc8d8b476c528c2a8a119089cca8f36b79
kmcguire727-debian-mac <kmcguire727@gmail.com>
2023-09-15 16:21:44 -0400

    cleanup

--- ds_salary_prj/salary_scraper.py
+++
@@ -1,4 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.get("https://www.selenium.dev/selenium/web/web-form.html")
--- student.py
+++
@@ -1,7 +0,0 @@
-class Student:  # a class is what a student is
-
-    def __init__(self, name, major, gpa, is_on_probation):
-        self.name = name
-        self.major = major
-        self.gpa = gpa
-        self.is_on_probation = is_on_probation

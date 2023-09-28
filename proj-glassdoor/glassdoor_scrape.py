34d7019c8d47c672aead6acbca8336cf0c18c38a
kmcguire727 <kmcguire727@gmail.com>
2023-09-27 22:18:27 -0400

    change project

---
+++ proj-glassdoor/glassdoor_scrape.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+    glassdoor_scrape.py: this file reuses work from Omer Sakarya to 
+    generate a DataFrame of content for analysis
+    Original work can be found here: https://mersakarya.medium.com/selenium-tutorial-scraping-/
+    					/glassdoor-com-in-10-minutes-3d0915c6d905
+"""
+
